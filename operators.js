b3fa5a780d7be42494086aad4057a5a0fcf2a8a4
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-02-25 14:51:00 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,97 @@
+var a=100;b=13;c=10;
+var linebreal="<br>";
+document.write("Arithmetic Operators:");
+document.write(linebreal);
+document.write(linebreal);
+
+document.write("a+b+c= ");
+var result =(a+b+c);
+document.write(result);
+document.write(linebreal);
+
+
+document.write("a+b= ");
+var result =(a+b);
+document.write(result);
+document.write(linebreal);
+
+document.write("a-b= ");
+var result =(a-b);
+document.write(result);
+document.write(linebreal);
+
+document.write("a*b= ");
+var result =(a*b);
+document.write(result);
+document.write(linebreal);
+
+document.write("a/b= ");
+var result =(a/b);
+document.write(result);
+document.write(linebreal);
+
+document.write("a%b= ");
+var result =(a%b);
+document.write(result);
+document.write(linebreal);
+document.write(linebreal);
+document.write("Comparison Operators:");
+document.write(linebreal);
+document.write(linebreal);
+
+var x=10;y=20;
+document.write("a==b: ");
+result=(x==y);
+document.write(result);
+document.write(linebreal);
+document.write("a<b: ");
+result=(x<y);
+document.write(result);
+document.write(linebreal);
+document.write("a>b: ");
+result=(x>y);
+document.write(result);
+document.write(linebreal);
+document.write("a!=b: ");
+result=(x!=y);
+document.write(result);
+document.write(linebreal);
+document.write("a>=b: ");
+result=(x>=y);
+document.write(result);
+document.write(linebreal);
+document.write("a<=b: ");
+result=(x<=y);
+document.write(result);
+document.write(linebreal);
+
+document.write(linebreal);
+document.write(linebreal);
+document.write("Assignment Operators:");
+document.write(linebreal);
+document.write(linebreal);
+var u=40;v=20;
+document.write("Value of: (a=b): ");
+result=(a=b);
+document.write(result);
+document.write(linebreal);
+document.write("Value of: (a+=b): ");
+result=(a+=b);
+document.write(result);
+document.write(linebreal);
+document.write("Value of: (a-=b): ");
+result=(a-=b);
+document.write(result);
+document.write(linebreal);
+document.write("Value of: (a*=b): ");
+result=(a*=b);
+document.write(result);
+document.write(linebreal);
+document.write("Value of: (a/=b): ");
+result=(a/=b);
+document.write(result);
+document.write(linebreal);
+document.write("Value of: (a%=b): ");
+result=(a%=b);
+document.write(result);
+document.write(linebreal);
