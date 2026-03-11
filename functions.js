c62a25454a1655a58a0ab5002d500e4b03f3006e
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-03-11 14:49:44 +0530

    Function

---
+++ functions.js
@@ -0,0 +1,14 @@
+function myFunction()
+{
+    alert("Hello World");
+}
+
+function sayHello(name,age)
+{
+    alert("Hello" + name);
+}
+let add= (a,b)=>
+{
+    document.write(a+b);
+}
+add(10,15);
